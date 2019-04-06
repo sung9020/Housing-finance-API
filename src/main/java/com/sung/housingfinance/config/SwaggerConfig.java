f02c20e31c223c9b39fb1f754e49f546a4b1c641
sung9020 <123msn@naver.co.kr>
2019-04-06 20:04:06 +0900

    스웨거 리스트 오픈 적용
    Signed-off-by: sung9020 <123msn@naver.co.kr>

--- src/main/java/com/sung/housingfinance/config/SwaggerConfig.java
+++ src/main/java/com/sung/housingfinance/config/SwaggerConfig.java
@@ -20,6 +20,9 @@ import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
@@ -67,4 +70,10 @@ public class SwaggerConfig {
                 authorizationScopes));
     }
 
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .docExpansion(DocExpansion.LIST) // or DocExpansion.NONE or DocExpansion.FULL
+                .build();
+    }
 }
